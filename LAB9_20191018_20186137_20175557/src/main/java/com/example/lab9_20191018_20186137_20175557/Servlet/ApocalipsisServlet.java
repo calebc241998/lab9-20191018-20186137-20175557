6b381415ff80eaba85c691a208fa7b3243d012d3
Christopher Terrones <a20182048@pucp.edu.pe>
2022-06-21 19:36:49 -0500

    Servlet avance 1 listo

---
+++ LAB9_20191018_20186137_20175557/src/main/java/com/example/lab9_20191018_20186137_20175557/Servlet/ApocalipsisServlet.java
@@ -0,0 +1,44 @@
+package com.example.lab9_20191018_20186137_20175557.Servlet;
+
+import com.example.lab9_20191018_20186137_20175557.Daos.DaoApocalipsis;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ApocalipsisServlet", value = "/ApocalipsisServlet")
+public class ApocalipsisServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DaoApocalipsis daoApocalipsis = new DaoApocalipsis();
+        String action = request.getParameter("a") == null ? "listar" : request.getParameter("a");
+
+        switch(action){
+            case "listar" -> {
+                request.setAttribute("Mision1",daoApocalipsis.Mision1());
+
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("Apocalipsis.jsp");
+                requestDispatcher.forward(request,response);
+            }
+            case "zombies" -> {
+                //request.setAttribute("Mision4",daoApocalipsis.Mision4());
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("Zombies.jsp");
+                requestDispatcher.forward(request,response);
+            }
+            case "supervivientes" -> {
+                //request.setAttribute("Mision4",daoApocalipsis.Mision2());
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("Supervivientes.jsp");
+                requestDispatcher.forward(request,response);
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+
